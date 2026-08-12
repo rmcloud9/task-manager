e9a9a2c039e5470bf3fc3c073d179f968f7f4ba6
Ronak Mohapatra <ronakmohapatra605@gmail.com>
2026-08-12 07:52:55 +0530

    Improving 'add' option and changing overall code structure

--- task_manager.py
+++ task_manager.py
@@ -1,56 +1,56 @@
 while True:
     user_input = input("Type add / show / edit / complete / exit:")
 
-    match user_input:
-        case 'add':
-            todo = input("Enter a new todo:") + "\n"
+    if 'add' in user_input:
+        todo = input("Enter a new todo:") + "\n"
+        todo = todo[4:]
 
-            with open('todos.txt' ,'r') as file:
-                todos = file.readlines()
-            
-            todos.append(todo)
+        with open('todos.txt' ,'r') as file:
+            todos = file.readlines()
+        
+        todos.append(todo)
 
-            with open('todos.txt','w') as file:
-                file.writelines(todos)
+        with open('todos.txt','w') as file:
+            file.writelines(todos)
 
-        case 'show':
-            with open('todos.txt' ,'r') as file:
-                todos = file.readlines()
-            # todos = [todo.strip('\n') for todo in todos]       
-            for index, item in enumerate(todos):
-                item = item.strip('\n')
-                row = f"{index + 1}.{item}"
-                print(row)
+    if 'show' in user_input:
+        with open('todos.txt' ,'r') as file:
+            todos = file.readlines()
+        # todos = [todo.strip('\n') for todo in todos]       
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index + 1}.{item}"
+            print(row)
 
-        case 'edit':
-            with open('todos.txt' ,'r') as file:
-                todos = file.readlines()
+    if 'edit' in user_input:
+        with open('todos.txt' ,'r') as file:
+            todos = file.readlines()
 
-            index = int(input("Enter the serial number of todo you want to edit:"))
-            new_todo = input("Enter the modified todo:")
-            todos[index - 1] = new_todo + "\n"
+        index = int(input("Enter the serial number of todo you want to edit:"))
+        new_todo = input("Enter the modified todo:")
+        todos[index - 1] = new_todo + "\n"
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
 
-        case 'complete':
-            number = int(input("Enter the serial number of todo you want to complete:"))
-            index = number - 1
+    if 'complete' in user_input:
+        number = int(input("Enter the serial number of todo you want to complete:"))
+        index = number - 1
 
-            with open('todos.txt' ,'r') as file:
-                todos = file.readlines()
+        with open('todos.txt' ,'r') as file:
+            todos = file.readlines()
 
-            todo_to_remove = todos[index].strip('\n')
-            todos.pop(index)
+        todo_to_remove = todos[index].strip('\n')
+        todos.pop(index)
 
-            with open('todos.txt','w') as file:
-                file.writelines(todos)
+        with open('todos.txt','w') as file:
+            file.writelines(todos)
 
-            message = f"Todo {todo_to_remove} has been removed from the list."
-            print(message)
+        message = f"Todo {todo_to_remove} has been removed from the list."
+        print(message)
 
-        case 'exit':
-            break
+    if 'exit' in user_input:
+        break
 
 print('Goodbye')
 
