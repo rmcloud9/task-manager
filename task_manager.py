c10cf7fcf6c80d7fbde1642755802807e8c1ae97
Ronak Mohapatra <ronakmohapatra605@gmail.com>
2026-08-13 23:37:52 +0530

    Optimised code(using if-else and reducing number of times input needed)

--- task_manager.py
+++ task_manager.py
@@ -13,7 +13,7 @@ while True:
         with open('todos.txt','w') as file:
             file.writelines(todos)
 
-    if 'show' in user_input:
+    elif 'show' in user_input:
         with open('todos.txt' ,'r') as file:
             todos = file.readlines()
         # todos = [todo.strip('\n') for todo in todos]       
@@ -22,20 +22,19 @@ while True:
             row = f"{index + 1}.{item}"
             print(row)
 
-    if 'edit' in user_input:
+    elif 'edit' in user_input:
         with open('todos.txt' ,'r') as file:
             todos = file.readlines()
 
-        index = int(input("Enter the serial number of todo you want to edit:"))
+        index = int(user_input[5:])
         new_todo = input("Enter the modified todo:")
         todos[index - 1] = new_todo + "\n"
 
         with open('todos.txt', 'w') as file:
             file.writelines(todos)
 
-    if 'complete' in user_input:
-        number = int(input("Enter the serial number of todo you want to complete:"))
-        index = number - 1
+    elif 'complete' in user_input:
+        index = int(user_input[9:]) - 1
 
         with open('todos.txt' ,'r') as file:
             todos = file.readlines()
@@ -49,8 +48,11 @@ while True:
         message = f"Todo {todo_to_remove} has been removed from the list."
         print(message)
 
-    if 'exit' in user_input:
+    elif 'exit' in user_input:
         break
+
+    else:
+        print("Invalid command")
 
 print('Goodbye')
 
