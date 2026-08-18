9fcf6fab740b43aae4d8ec0535b1f1b0ee490532
Ronak Mohapatra <ronakmohapatra605@gmail.com>
2026-08-18 22:42:59 +0530

    Optimizing code by adding functions with parameters

--- task_manager.py
+++ task_manager.py
@@ -1,8 +1,11 @@
-def get_todos():
-    with open('todos.txt' ,'r') as file:
+def get_todos(filepath):
+    with open(filepath ,'r') as file:
         todos_local = file.readlines()
     return todos_local
 
+def write_todos(filepath, new_todos):
+    with open(filepath,'w') as file:
+        file.writelines(new_todos)
 
 while True:
     user_input = input("Type add / show / edit / complete / exit:")
@@ -10,15 +13,14 @@ while True:
     if user_input.startswith('add'):
         todo = user_input[4:]
 
-        todos = get_todos()
+        todos = get_todos("todos.txt")
         
         todos.append(todo + '\n')
 
-        with open('todos.txt','w') as file:
-            file.writelines(todos)
+        write_todos("todos.txt", todos)
 
     elif user_input.startswith('show'):
-        todos = get_todos()
+        todos = get_todos("todos.txt")
         # todos = [todo.strip('\n') for todo in todos]       
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -33,26 +35,24 @@ while True:
             print("Invalid input format")
             continue
 
-        todos = get_todos()
+        todos = get_todos("todos.txt")
 
         index = index - 1
         new_todo = input("Enter the modified todo:")
         todos[index] = new_todo + "\n"
         
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+        write_todos("todos.txt", todos)
 
     elif user_input.startswith('complete'):
         try:
             index = int(user_input[9:]) - 1
 
-            todos = get_todos()
+            todos = get_todos("todos.txt")
 
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            with open('todos.txt','w') as file:
-                file.writelines(todos)
+            write_todos("todos.txt", todos)
 
             message = f"Todo {todo_to_remove} has been removed from the list."
             print(message)
