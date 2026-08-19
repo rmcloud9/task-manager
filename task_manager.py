6893c748b796bd92489b1894fb62011af8714a08
Ronak Mohapatra <ronakmohapatra605@gmail.com>
2026-08-19 10:53:26 +0530

    Improving code by using default arguements in functions and using docstrings

--- task_manager.py
+++ task_manager.py
@@ -1,9 +1,13 @@
-def get_todos(filepath):
+def get_todos(filepath='todos.txt'):
+    """Read the text file and 
+    return the list of to-do items.
+    """
     with open(filepath ,'r') as file:
         todos_local = file.readlines()
     return todos_local
 
-def write_todos(filepath, new_todos):
+def write_todos(new_todos, filepath='todos.txt'):
+    """Write the to-do items list in a text file"""
     with open(filepath,'w') as file:
         file.writelines(new_todos)
 
@@ -13,15 +17,15 @@ while True:
     if user_input.startswith('add'):
         todo = user_input[4:]
 
-        todos = get_todos("todos.txt")
+        todos = get_todos()
         
         todos.append(todo + '\n')
 
-        write_todos("todos.txt", todos)
+        write_todos(todos)
 
     elif user_input.startswith('show'):
-        todos = get_todos("todos.txt")
-        # todos = [todo.strip('\n') for todo in todos]       
+        todos = get_todos()
+        # todos = [todo.strip('\n') for todo in todos]  can use list comprehension in code     
         for index, item in enumerate(todos):
             item = item.strip('\n')
             row = f"{index + 1}.{item}"
@@ -35,24 +39,24 @@ while True:
             print("Invalid input format")
             continue
 
-        todos = get_todos("todos.txt")
+        todos = get_todos()
 
         index = index - 1
         new_todo = input("Enter the modified todo:")
         todos[index] = new_todo + "\n"
         
-        write_todos("todos.txt", todos)
+        write_todos(todos)
 
     elif user_input.startswith('complete'):
         try:
             index = int(user_input[9:]) - 1
 
-            todos = get_todos("todos.txt")
+            todos = get_todos()
 
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos("todos.txt", todos)
+            write_todos(todos)
 
             message = f"Todo {todo_to_remove} has been removed from the list."
             print(message)
@@ -72,4 +76,3 @@ while True:
         print("Invalid command")
 
 print('Goodbye')
-
